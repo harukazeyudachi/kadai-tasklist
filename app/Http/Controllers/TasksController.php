5f06a9c02b72046b58a668e4821393d425ef69cb
harukaze.yudachi <kirameki0hanako@gmail.com>
2019-05-12 08:57:31 +0000

    delete

--- app/Http/Controllers/TasksController.php
+++ app/Http/Controllers/TasksController.php
@@ -142,6 +142,7 @@ class TasksController extends Controller
      */
     public function destroy($id)
     {
+        $task = \App\Task::find($id);
         if (\Auth::id() === $task->user_id) {
         $task->delete();
         }
